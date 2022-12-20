b8bb0984fb4e73846b5b25800737069f76551afd
Raul Elguezabal <jre0310@gmail.com>
2022-12-20 16:55:52 -0600

    Initial Commit

---
+++ Shared/Max_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Max_TrackerApp.swift
+//  Shared
+//
+//  Created by Raul Elguezabal on 12/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Max_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
